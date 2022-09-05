6b8c186ec424829b258a9a33a7a03f9f354c94d1
mauroturner <mauroandresturner@gmail.com>
2022-09-05 16:35:33 -0300

    Obtención de ministerios, localidades

--- Main.py
+++ Main.py
@@ -20,13 +20,38 @@ def main():
         if os.path.isfile(f):
             # Obtenemos los datos y ordenamos
             df = pd.read_excel(f, header=0).sort_values('MINISTERIO / ENTE / ORGANISMO')
-            #print(df.sort_values('MINISTERIO / ENTE / ORGANISMO'))
-            # Creamos un diccionario de ministerios
-            ministerios = dict.fromkeys(df['MINISTERIO / ENTE / ORGANISMO'].unique(), {'LOCALIDADES': 'rio gallegos'})
-            #test = df.loc[df['MINISTERIO / ENTE / ORGANISMO'].isin(['ASIP'])]
-            #print(test)
+
+            # Listado de ministerios, localidades, puestos y género
+            ministerios = df['MINISTERIO / ENTE / ORGANISMO'].unique()
+            localidades = df['LOCALIDAD'].unique()
+            puestos = df['PUESTO ACTUAL'].unique()
+            generos = df['GÉNERO'].unique()
+
+            # Diccionario de estadísticas
+            estadisticas = dict.fromkeys(df['MINISTERIO / ENTE / ORGANISMO'].unique(), {})
+ 
+            # Creamos las estadísticas
             for ministerio in ministerios:
-                print(ministerio)
+                informacion_ministerio = df.loc[df['MINISTERIO / ENTE / ORGANISMO'].isin([ministerio])]
+                ministerio_localidad = informacion_ministerio['LOCALIDAD'].unique()
+                ministerio_localidad_funcion = informacion_ministerio['LOCALIDAD'].loc[informacion_ministerio['PUESTO ACTUAL'].isin([puestos])].unique()
+
+                #print(ministerio_localidad)
+                estadisticas[ministerio]["ESTADISTICAS"] = {
+                    'LOCALIDADES': ministerio_localidad,
+                    'FUNCION': ministerio_localidad_funcion,
+
+                }
+            print(estadisticas["ASIP"]["ESTADISTICAS"])
+            #filtro_ministerio = df.loc[df['MINISTERIO / ENTE / ORGANISMO'].isin(['AMA'])]
+            #localidades = filtro_ministerio['LOCALIDAD']
+            #filtro_localidad = filtro_ministerio.loc[filtro_ministerio['PUESTO ACTUAL'].isin(['a'])]
+            #print(filtro_localidad)
+            #for t in test:
+            #    print(t)
+            #print(test)
+            #for ministerio in ministerios:
+            #    ministerios[ministerio]
 
 if __name__ == "__main__":
     main()
