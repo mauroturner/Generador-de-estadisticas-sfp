19391195b758a063fc2f653ab02330a4077dab7f
mauroturner <mauroandresturner@gmail.com>
2022-09-06 06:10:10 -0300

    Generador de estadisticas base

--- Main.py
+++ Main.py
@@ -1,7 +1,8 @@
+from multiprocessing.util import info
 import numpy as np
 import openpyxl
 import pandas as pd
-import os 
+import os
 
 """
 Estadísticas por 
@@ -23,35 +24,58 @@ def main():
 
             # Listado de ministerios, localidades, puestos y género
             ministerios = df['MINISTERIO / ENTE / ORGANISMO'].unique()
-            localidades = df['LOCALIDAD'].unique()
-            puestos = df['PUESTO ACTUAL'].unique()
-            generos = df['GÉNERO'].unique()
 
-            # Diccionario de estadísticas
-            estadisticas = dict.fromkeys(df['MINISTERIO / ENTE / ORGANISMO'].unique(), {})
- 
-            # Creamos las estadísticas
+            # Agrupamos por ministerio
+            estadisticas_por_ministerio = df.groupby('MINISTERIO / ENTE / ORGANISMO')
+            wb = openpyxl.Workbook()
+            hoja = wb.active
+            hoja['A1'].value = "Curso: " + archivo.split('.')[0]
+            hoja['B1'].value = "Total: " + str(len(df.index))
+            i = 5
+
+            # TO-DO
+            # Selección de campos
+            # Estilos 
+            # Merge de celdas
+
+            # Sin género
             for ministerio in ministerios:
-                informacion_ministerio = df.loc[df['MINISTERIO / ENTE / ORGANISMO'].isin([ministerio])]
-                ministerio_localidad = informacion_ministerio['LOCALIDAD'].unique()
-                ministerio_localidad_funcion = informacion_ministerio['LOCALIDAD'].loc[informacion_ministerio['PUESTO ACTUAL'].isin([puestos])].unique()
+                estadisticas_ministerio = estadisticas_por_ministerio.get_group(ministerio).agg('value_counts')
+                localidades = estadisticas_ministerio.reset_index(name='TOTAL')['LOCALIDAD']
+                funciones =  estadisticas_ministerio.reset_index(name='TOTAL')['PUESTO ACTUAL']
+                totales = estadisticas_ministerio.reset_index(name='TOTAL')['TOTAL']
+                print(estadisticas_ministerio)
+                print('--------------' + ministerio + '----------------')
+                hoja['A' + str(i)].value = ministerio
+                for localidad, funcion, total in zip(localidades, funciones, totales):
+                    hoja['B' + str(i)].value = localidad
+                    hoja['C' + str(i)].value = funcion
+                    hoja['D' + str(i)].value = total
+                    i += 1
 
-                #print(ministerio_localidad)
-                estadisticas[ministerio]["ESTADISTICAS"] = {
-                    'LOCALIDADES': ministerio_localidad,
-                    'FUNCION': ministerio_localidad_funcion,
-
-                }
-            print(estadisticas["ASIP"]["ESTADISTICAS"])
-            #filtro_ministerio = df.loc[df['MINISTERIO / ENTE / ORGANISMO'].isin(['AMA'])]
-            #localidades = filtro_ministerio['LOCALIDAD']
-            #filtro_localidad = filtro_ministerio.loc[filtro_ministerio['PUESTO ACTUAL'].isin(['a'])]
-            #print(filtro_localidad)
-            #for t in test:
-            #    print(t)
-            #print(test)
-            #for ministerio in ministerios:
-            #    ministerios[ministerio]
+            # Con género
+            # for ministerio in ministerios:
+            #      estadisticas_ministerio = estadisticas_por_ministerio.get_group(ministerio).agg('value_counts')
+            #      localidades = estadisticas_ministerio.reset_index(name='TOTAL')['LOCALIDAD']
+            #      funciones =  estadisticas_ministerio.reset_index(name='TOTAL')['PUESTO ACTUAL']
+            #      generos = estadisticas_ministerio.reset_index(name='TOTAL')['GÉNERO']
+            #      totales = estadisticas_ministerio.reset_index(name='TOTAL')['TOTAL']
+            #      print('--------------' + ministerio + '----------------')
+            #      hoja['A' + str(i)].value = ministerio
+            #      for localidad, funcion, genero, total in zip(localidades, funciones, generos, totales):
+            #         hoja['B' + str(i)].value = localidad
+            #         hoja['C' + str(i)].value = funcion
+            #         if genero == 'MASCULINO':
+            #             hoja['D' + str(i)].value = total
+            #             hoja['E' + str(i)].value = 0
+            #         else:
+            #             hoja['D' + str(i)].value = 0
+            #             hoja['E' + str(i)].value = total
+            #         i += 1
+            
+            wb.save(excel_output)
+            excel_input = "Archivos\\Entrada"
+            excel_output = "Archivos\\Salida"
 
 if __name__ == "__main__":
     main()
